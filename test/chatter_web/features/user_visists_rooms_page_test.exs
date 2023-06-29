1263cce7d83b5bd9ebeeb9c4c38a145c200879d5
Fabrice Claeys <fabrice.claeys@symbol-it.fr>
2023-06-29 11:43:34 +0200

    refacto list of rooms

--- test/chatter_web/features/user_visists_rooms_page_test.exs
+++ test/chatter_web/features/user_visists_rooms_page_test.exs
@@ -1,13 +1,20 @@
 defmodule ChatterWeb.UserVisitsRoomsPageTest do
   use ChatterWeb.FeatureCase, async: true
 
+  @room_index "/"
+
   describe "as a user, I would like to see a list of all available chats" do
     test "user visists rooms page to see a list offf rooms", %{session: session} do
-      room = insert(:chat_room)
+      [room1, room2] = insert_pair(:chat_room)
 
       session
-      |> visit("/")
-      |> assert_has(Query.css(".room", text: room.name))
+      |> visit(@room_index)
+      |> assert_has(room_name(room1))
+      |> assert_has(room_name(room2))
     end
+  end
+
+  defp room_name(room) do
+    Query.data("role", "room", text: room.name)
   end
 end
